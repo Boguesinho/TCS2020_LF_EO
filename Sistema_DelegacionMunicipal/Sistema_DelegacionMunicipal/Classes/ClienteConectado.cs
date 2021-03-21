a629f2b6aa99fc89718269dcf820977afa9df795
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2021-03-20 20:38:25 -0600

    CambiosPrueba

--- Sistema_DelegacionMunicipal/Sistema_DelegacionMunicipal/Classes/ClienteConectado.cs
+++ Sistema_DelegacionMunicipal/Sistema_DelegacionMunicipal/Classes/ClienteConectado.cs
@@ -8,12 +8,15 @@ namespace Sistema_DelegacionMunicipal.Classes
 {
     class ClienteConectado
     {
+        public List<string> lisNueva = new List<string>();
         public List<string> usuariosEmisores = new List<string>();
         public List<string> delegacionesEmisores = new List<string>();
         public List<string> direccionesUsuario = new List<string>();
         public bool isReporte { get; set; }
         public bool isMensaje { get; set; }
+        public bool booleanoNuevo { get; set; }
 
+        public bool booleanoNuevo2 { get; set; }
         public ClienteConectado(List<string> usuariosEmisores, List<string> delegacionesEmisores, List<string> direccionesUsuario)
         {
             this.usuariosEmisores = usuariosEmisores;
